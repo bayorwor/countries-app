cebefe79ab3961dae1ef4f225261d7a8f37a2d45
bayorwor <bayorwor60@gmail.com>
2021-03-23 17:35:25 +0000

    added some styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <section
+      className="bg-secondary d-flex flex-column justify-content-center align-items-center"
+      style={{ height: "100vh" }}
+    >
+      <div className="container text-white">
+        <h1 className="mb-5 text-warning">Know Your Countries</h1>
+        <h2>
+          This site allows you to have access to <br></br>all the countries
+          around the Globe
+        </h2>
+        <Link to="/countries" className="btn btn-info">
+          SEE MORE
+        </Link>
+      </div>
+    </section>
   );
 }
 
