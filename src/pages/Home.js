b3f24807ab2738af2386b0a0b0f3a1e34421854d
bayorwor <bayorwor60@gmail.com>
2021-03-25 09:18:46 +0000

    added a legend img

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,18 +3,21 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <section
-      className="bg-secondary d-flex flex-column justify-content-center align-items-center"
+      className="bg-secondary d-flex align-items-center"
       style={{ height: "100vh" }}
     >
-      <div className="container text-white">
-        <h1 className="mb-5 text-warning">Know Your Countries</h1>
-        <h2>
-          This site allows you to have access to <br></br>all the countries
-          around the Globe
-        </h2>
-        <Link to="/countries" className="btn btn-info">
-          SEE MORE
-        </Link>
+      <div className="container text-white  d-flex justify-content-between align-items-center">
+        <div className="text-white">
+          <h1 className="mb-5 text-warning">Know Your Countries</h1>
+          <h2>
+            This site allows you to have access to <br></br>all the countries
+            around the Globe
+          </h2>
+          <Link to="/countries" className="btn btn-info">
+            SEE MORE
+          </Link>
+        </div>
+        <img src="./globe.svg" alt="legend" style={{ height: "50vh" }} />
       </div>
     </section>
   );
